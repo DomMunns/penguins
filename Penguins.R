c77347cb38f7f1ecfbbe9e25fbd464e4d30cb24a
DomMunns <dem520@york.ac.uk>
2020-11-18 12:44:24 +0000

    penguins pca

---
+++ Penguins.R
@@ -0,0 +1,54 @@
+library(tidyverse)
+
+# load package to access the data
+library(palmerpenguins)
+
+# clean variable names
+penguin <- penguins_raw %>%
+  janitor::clean_names()
+
+# do a summary to look at the four varibales used
+penguin %>%
+  select(body_mass_g,
+         ends_with("_mm")) %>%
+  summary()
+
+# filter out the rows with mising values
+penguin <- penguin %>%
+  filter(!is.na(body_mass_g))
+
+# split species into common_name and scientific_name
+penguin <- penguin %>% 
+  extract(species, 
+          c("common_name", "scientific_name"),
+          "([a-zA-Z]+\\s[a-zA-Z]+)\\s\\(([a-zA-Z]+\\s[a-zA-Z]+)\\)")
+
+# load package GGally
+library(GGally)
+
+# select variables of interest and pipe in to ggpairs()
+penguin %>%
+  select(common_name, 
+         sex, 
+         island,
+         body_mass_g,
+         ends_with("_mm")) %>%
+  ggpairs(aes(color = common_name))
+
+# select the four variables and do PCA
+pca <- penguin %>% 
+  select(body_mass_g,
+         ends_with("_mm")) %>%
+  prcomp(scale. = TRUE)
+
+summary(pca)
+pca$rotation
+
+# Extract the scores into a dataframe with the species name
+pca_labelled <- data.frame(pca$x, common_name = penguin$common_name)
+# scatterplot
+pca_labelled %>% 
+  ggplot(aes(x = PC1, y = PC2, color = common_name)) +
+  geom_point()
+
+
